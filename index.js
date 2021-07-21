cafb9ed7f1e27c5c661a806deb4dabac815b7055
yamaguchi371 <telework-takefu6@outlook.jp>
2021-07-21 13:25:24 +0900

    express end

--- index.js
+++ index.js
@@ -9,31 +9,52 @@ app.use(express.static('public'));
 var bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended: false}));
 
+var data = {
+    'Taro': 'taro@yamada',
+    'Hanako': 'hanako@flower',
+    'Sachiko': 'sachiko@happy',
+    'Ichiro': 'ichiro@baseball',
+};
+
 // ※トップページ
 app.get('/', (req, res) => {
-    var msg = 'This is Express Page!<br>'
-        + '※メッセージを書いて送信して下さい。';
+    var msg = 'This is Index Page!<br>'
+        + '※データを表示します。';
     res.render('index.ejs',
         {
             title: 'Index',
             content: msg,
+            data: data,
         });
 });
 
-// ※otherページ
-app.get("/other", (req, res) => {
-    var name = req.query.name;
-    var pass = req.query.pass;
-    var msg = 'あなたの名前は「' + name +
-        '」<br>パスワードは「' + pass + '」です。';
+// ※POST送信の処理
+app.post('/', (req,res) => {
+    var msg = 'This is Posted Page!<br>' +
+        'あなたは「<b>' + req.body.message +
+        '</b>」と送信しました。';
     res.render('index.ejs',
         {
-            title: 'other',
+            title: 'Posted',
             content: msg,
-            link:{href:'/', text:'※トップに戻る'}
         }
     );
 });
+
+// ※otherページ
+// app.get("/other", (req, res) => {
+//     var name = req.query.name;
+//     var pass = req.query.pass;
+//     var msg = 'あなたの名前は「' + name +
+//         '」<br>パスワードは「' + pass + '」です。';
+//     res.render('index.ejs',
+//         {
+//             title: 'other',
+//             content: msg,
+//             link:{href:'/', text:'※トップに戻る'}
+//         }
+//     );
+// });
 
 app.listen(3000, () => {
     console.log('Server is running!');
